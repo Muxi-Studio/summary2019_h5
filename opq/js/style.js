f050306ef804868f40aa243fbb251dc24f4d4b93
jonusis <jonusis@github.com>
2020-01-11 12:02:31 +0800

    feat:new page

---
+++ opq/js/style.js
@@ -0,0 +1,11 @@
+document.write('<link rel="stylesheet" href="css/box1.css"/>');
+document.write('<link rel="stylesheet" href="css/box2.css"/>');
+document.write('<link rel="stylesheet" href="css/box3.css"/>');
+document.write('<link rel="stylesheet" href="css/box4.css"/>');
+document.write('<link rel="stylesheet" href="css/box5.css"/>');
+document.write('<link rel="stylesheet" href="css/box6.css"/>');
+document.write('<link rel="stylesheet" href="css/box7.css"/>');
+document.write('<link rel="stylesheet" href="css/box8.css"/>');
+document.write('<link rel="stylesheet" href="css/box9.css"/>');
+document.write('<link rel="stylesheet" href="css/index.css"/>');
+document.write('<link rel="stylesheet" href="css/iSlider.css"/>');
