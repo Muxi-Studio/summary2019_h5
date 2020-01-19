c82fcca85fa99f999a1379957e12e762e6ea6d8f
jonusis <jonusis@github.com>
2020-01-19 17:25:00 +0800

    feat:new func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import html2canvas from "html2canvas";
+import './App.css';
+import './file/bank.png';
+import './file/myfont.ttf';
+import './file//music/暂停.png';
+import './file//music/音乐播放.png';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 将`body`节点截取变为canvas
+html2canvas(document.querySelector("body"), {
+    dpi: window.devicePixelRatio * 2, // 对应屏幕的dpi，适配高清屏，解决canvas模糊问题
+    scale: 2, // 缩放
+    useCORS: true
+    // y: window.scrollY // 根据滚动条来截取--主要用于截取某一个区域
+}).then(canvas =>{
+        //canvas参数为生成的canvas的dom节点，可以对其进行dom操作
+        let dataUrl = canvas.toDataURL("image/png", 1.0),
+        newImg = document.createElement("img");
+        newImg.setAttribute("crossOrigin",'anonymous');
+        newImg.src = dataUrl;
+        document.body.appendChild(newImg);
+        newImg.style.width = '100%';
+        // newImg.style.position = 'absolute';
+        // newImg.style.top = '0';
+        console.log(newImg);
+});
